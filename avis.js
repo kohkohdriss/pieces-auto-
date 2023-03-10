b7b79f3faed17843f586ff7f8b448520b0d02c1e
Driss KOHKOH <kohkoh.driss@gmail.com>
2023-03-10 09:57:41 +0100

    ajout d'avis

--- avis.js
+++ avis.js
@@ -9,6 +9,7 @@ export function ajoutListenersAvis() {
       );
       const avis = await reponse.json();
       const pieceElement = event.target.parentElement;
+
       const avisElement = document.createElement("p");
       for (let i = 0; i < avis.length; i++) {
         avisElement.innerHTML += `<b>${avis[i].utilisateur}:</b> ${avis[i].commentaire} <br>`;
@@ -16,4 +17,28 @@ export function ajoutListenersAvis() {
       pieceElement.appendChild(avisElement);
     });
   }
+}
+
+export function ajoutListenerEnvoyerAvis() {
+  const formulaireAvis = document.querySelector(".formulaire-avis");
+  formulaireAvis.addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Création de l’objet du nouvel avis.
+    console.log("event****************", event);
+    const avis = {
+      pieceId: parseInt(event.target.querySelector("[name=piece-id]").value),
+      utilisateur: event.target.querySelector("[name=utilisateur").value,
+      commentaire: event.target.querySelector("[name=commentaire]").value,
+      nbEtoiles: parseInt(event.target.querySelector("[name=nbEtoiles]").value),
+    };
+
+    // Création de la charge utile au format JSON
+    const chargeUtile = JSON.stringify(avis);
+    // Appel de la fonction fetch avec toutes les informations nécessaires
+    fetch("http://localhost:8081/avis", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: chargeUtile,
+    });
+  });
 }
